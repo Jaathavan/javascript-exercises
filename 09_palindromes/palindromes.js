e45cd61d1fb198f8d93401393d631efe5827d19f
Jaathavan Ranjanathan <81396102+Jaathavan@users.noreply.github.com>
2022-06-23 10:33:04 -0400

    "Completed palindrom"

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str.reduce((palindrome, currentLetter, currentIndex, str) => {
+        currentLetter.match(/[a-z]/i);
+    }, "")
+    return str.split("").reverse().join("");
 };
 
 // Do not edit below this line
