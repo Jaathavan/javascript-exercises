34d8053f010bd4a9690128dc37fb1d77b1d2c96d
Jaathavan <rjaathu@outlook.com>
2022-06-23 18:43:18 -0400

    all changes updated

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...rem) {
+    let arr = array;
+    return array.filter(remove => !rem.includes(remove));
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let sum=0;
+    if (a>0 && b>0 && typeof(a)=="number" && typeof(b)==="number") {
+        let x = Math.max(a, b)
+        let y = Math.min(a, b)
+        for (let i=y; i <= x; i++) {
+            sum+=i;
+        }
+    }
+    else {
+        return "ERROR";
+    }
+    return sum;
 };
 
 // Do not edit below this line
